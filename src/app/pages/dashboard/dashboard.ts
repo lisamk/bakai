6c6c60f3b0e86e132fc62ad0e9f48ff9c6cd177a
Lisa Maria Ortner <kritzinger@gmx.net>
2026-04-04 21:04:28 +0200

    mkv mover change to lokal

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,15 +1,14 @@
-import { Component } from '@angular/core';
-import { Mkvmoverwidget } from './components/mkvmoverwidget/mkvmoverwidget';
+import {Component} from '@angular/core';
 
 @Component({
     selector: 'app-dashboard',
-    imports: [Mkvmoverwidget],
     template: `
         <div class="grid grid-cols-12 gap-8">
             <div class="col-span-12 lg:col-span-6 xl:col-span-4">
-                <app-mkvmoverwidget></app-mkvmoverwidget>
+                Leer
             </div>
         </div>
     `
 })
-export class Dashboard {}
+export class Dashboard {
+}
